e7334fe9c8cd1093f2cb3f6421abd7fe94c7ad76
andan <2864319823@qq.com>
2018-04-06 11:41:01 +0800

    Update ListToArray.java

--- ListToArray.java
+++ ListToArray.java
@@ -1,37 +1,37 @@
 	public static void countSameNumber1(int[] number){  
-        HashMap<Integer,Integer> hm = new HashMap<Integer,Integer>();  
-        for(int i =0;i < number.length;i++){  
-            if(!hm.containsKey(number[i])){  
-                hm.put(number[i],1);  
-            }  
-            else{  
-                hm.put(number[i],(hm.get(number[i]))+1);  
-            }  
-        }  
-        for(Entry<Integer, Integer> entry:hm.entrySet()){  
-            System.out.println(entry.getKey() + "出现了" + entry.getValue() + "次");  
-        }  
-    }
+		HashMap<Integer,Integer> hm = new HashMap<Integer,Integer>();  
+		for(int i =0;i < number.length;i++){  
+		    if(!hm.containsKey(number[i])){  
+			hm.put(number[i],1);  
+		    }  
+		    else{  
+			hm.put(number[i],(hm.get(number[i]))+1);  
+		    }  
+		}  
+		for(Entry<Integer, Integer> entry:hm.entrySet()){  
+		    System.out.println(entry.getKey() + "出现了" + entry.getValue() + "次");  
+		}  
+    	}
 	public static void countSameNumber2(int[] number){  
-        Arrays.sort(number);  
-        //若数组内为1 2 3 4 1 2 ,则创建一个大小为5的数组，下标为0-4.  
-        int[] numbers = new int[number[number.length -1] + 1];  
-        for(int i =0;i< numbers.length;i++){  
-            numbers[i] = 0;  
-        }  
-        //i即为原数组中数字，numbers[i]存放重复的次数  
-        for(int i =0; i<number.length;i++){  
-            int temp = number[i];  
-            numbers[temp]++;  
-        }  
-        //打印  
-        for(int i =0;i < numbers.length;i++){  
-            if(numbers[i] == 0){  
-                continue;  
-            }  
-            System.out.println(i + "出现了" + numbers[i] + "次");  
-        }  
-    }
+		Arrays.sort(number);  
+		//若数组内为1 2 3 4 1 2 ,则创建一个大小为5的数组，下标为0-4.  
+		int[] numbers = new int[number[number.length -1] + 1];  
+		for(int i =0;i< numbers.length;i++){  
+		    numbers[i] = 0;  
+		}  
+		//i即为原数组中数字，numbers[i]存放重复的次数  
+		for(int i =0; i<number.length;i++){  
+		    int temp = number[i];  
+		    numbers[temp]++;  
+		}  
+		//打印  
+		for(int i =0;i < numbers.length;i++){  
+		    if(numbers[i] == 0){  
+			continue;  
+		    }  
+		    System.out.println(i + "出现了" + numbers[i] + "次");  
+		}  
+   	 }
 	public static void countSameNumber3(int[] number){  
 	       ArrayList<Integer> al = new ArrayList<Integer>();  
 	       ArrayList<Integer> all = new ArrayList<Integer>();  
